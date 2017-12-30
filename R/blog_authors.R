72a7c5c622a438382425a5a201f72cceec8ef49e
Julian Flowers <julian.flowers12@gmail.com>
2017-12-30 18:15:03 +0000

    blog authors

---
+++ R/blog_authors.R
@@ -0,0 +1,24 @@
+#e# get blog authors
+
+get_blog_authors <- function(url = "https://publichealthmatters.blog.gov.uk"){
+  
+  
+  library(Rcrawler)
+  library(stringr)
+  library(tidyverse)
+  
+  getLinks <- LinkExtractor(url)[[2]]
+  
+  authors <- unique(getLinks[grepl("author",getLinks)])
+  
+  authors <- str_split(authors, "/", n  = 5)
+  
+  auths <- map_chr(authors, c(5,1))
+  
+  
+  
+  
+}
+
+
+
