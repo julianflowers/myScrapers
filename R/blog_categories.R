09d1df24f04c089c37a23e44e5181c03c9bf721c
Julian Flowers <julian.flowers12@gmail.com>
2017-12-30 18:11:54 +0000

    extract blog categories

---
+++ R/blog_categories.R
@@ -0,0 +1,24 @@
+#e# get blog categories
+
+get_blog_categories <- function(url = "https://publichealthmatters.blog.gov.uk"){
+  
+  
+  library(Rcrawler)
+  library(stringr)
+  library(tidyverse)
+  
+  getLinks <- LinkExtractor(url)[[2]]
+  
+  categories <- unique(getLinks[grepl("category",getLinks)])
+  
+  categories <- str_split(categories, "/", n  = 5)
+  
+  cats <- map_chr(categories, c(5,1))
+  
+  
+  
+  
+}
+
+cats <- get_blog_categories()
+head(cats)
