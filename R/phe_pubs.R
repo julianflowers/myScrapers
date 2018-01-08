1e8a0cdfe5269b86673b6cc19e75dc9b25e3f497
julianflowers <julianflowers@users.noreply.github.com>
2018-01-08 16:23:58 +0000

    Update phe_pubs.R

--- R/phe_pubs.R
+++ R/phe_pubs.R
@@ -1,68 +1,54 @@
 ## creates a data table of PHE stats and publications on .gov.uk
 
-phe_catalogue <- function(url = "https://www.gov.uk/government/publications?departments%5B%5D=public-health-england", pages = NULL) {
+phe_catalogue <- function(url = "https://www.gov.uk/government/publications?departments%5B%5D=public-health-england", pages = 2:90) {
   
-  
-  
-  library(Rcrawler)
-  
-  library(rvest)
-  
-  library(tidyverse)
-  
-  library(stringr)
-  
-  
-  #url <- url
-  
-  uplinks<- paste0(url,  pages, "&publication_filter_option=statistics")
-  
-  
-  
-  pubs <- map(uplinks, Rcrawler::LinkExtractor)
-  
-  pubs_links <- map(pubs, 2)
-  
-  pubs_counts <-map(pubs_links, as.data.frame)
-  
-  
-  
-  uplink <- map(Rcrawler::LinkExtractor(url)[2], as.data.frame)
-  
-  phe_pubs  <- as.data.frame(bind_rows(pubs_counts, uplink) )
-  
-  
-  colnames(phe_pubs) <- c("Links")
-  
-  
-  phe_national_pubs <- phe_pubs %>%
-    
-    mutate(group = case_when(str_detect(Links, "collections/")~ "collections",
-                             
-                             str_detect(Links, "statistics/") ~ "statistics",
-                             
-                             str_detect(Links, "publications/") ~ "publication"),
-           
-           link = paste0("<a href =",  Links,  ">Links</a>"))
+library(Rcrawler)
+library(rvest)
+library(tidyverse)
+library(stringr)
+
+
+first_page <- url
+sub_pages <- paste0(first_page,"&page=", 2:90)    
+#url <- url
+
+pubs <- map(sub_pages, Rcrawler::LinkExtractor)
+pubs <- map(pubs, 2) 
+pubs <-map(pubs, as.data.frame)
+
+uplink <- map(Rcrawler::LinkExtractor(first_page)[2], as.data.frame)
+phe_pubs  <- as.data.frame(rbind_list(pubs, uplink) )
+
+
+
+colnames(phe_pubs) <- c("Links")
+
+phe_pubs <- phe_pubs %>%
+  distinct()
+
+# createLink <- function(Links) {
+#   sprintf(paste0('<a href="', URLdecode(Links),'" target="_blank">', Links ,'</a>'))
+# }
+# 
+# createLink(phe_pubs$Links[1:3])
+phe_national_pubs <- phe_pubs %>%
+  mutate(group = case_when(str_detect(Links, "collections/")~ "collections", 
+                           str_detect(Links, "statistics/") ~ "statistics",
+                           str_detect(Links, "publications/") ~ "publication"), 
+         link = paste0("<a href =",  Links,  ">Links</a>"))
+
+#urls <- createLink(phe_national_pubs$Links[1:20])
+
+phe_national_pubs_table <- phe_national_pubs %>%
+  filter(!is.na(group)) %>%
+  DT::datatable(rownames = FALSE, escape = FALSE, extensions = 'Buttons', options = list(
+    dom = 'Bfrtip',
+    buttons = c('csv', 'excel', 'pdf')))
 
   
-  
-  phe_national_pubs_table <- phe_national_pubs %>%
-    
-    filter(!is.na(group)) %>%
-    
-    DT::datatable(rownames = FALSE, escape = FALSE, extensions = 'Buttons', options = list(
-      
-      dom = 'Bfrtip',
-      
-      buttons = c('csv', 'excel', 'pdf')))
-  
-  
+return(phe_national_pubs_table)  
 
-  return(phe_national_pubs_table) 
   
   
 }
-
-
 
