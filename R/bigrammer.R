ad7bc6a6e0ab8ce31cabd93aa82ebda9ba6c15bc
Julian Flowers <julian.flowers12@gmail.com>
2018-03-01 19:13:53 +0000

    update

--- R/bigrammer.R
+++ R/bigrammer.R
@@ -6,7 +6,7 @@ create_bigrams <- function(ds, text){
   require(tidyverse)
  text <- enquo(text)
   ds %>%
-    unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
+    unnest_tokens(bigram, !!text, token = "ngrams", n = 2) %>%
     separate(bigram, c("w1", "w2"), sep = " ") %>%
     filter(!w1 %in% stop_words$word) %>%
     filter(!w2 %in% stop_words$word) %>%
