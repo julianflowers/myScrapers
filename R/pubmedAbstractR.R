a9e69d304a33d2427cd6dc1ec6ae013a9751bd0a
Julian Flowers <julian.flowers12@gmail.com>
2018-03-09 13:17:48 -0800

    includes bibliometrix

--- R/pubmedAbstractR.R
+++ R/pubmedAbstractR.R
@@ -4,6 +4,7 @@ pubmedAbstractR <- function(search, n = 1000, start = 2000, end = end, db = "pub
   
   require(RISmed)
   require(tidyverse)
+  require(bibliometrix)
   
   search <- search
   n <- n
@@ -17,22 +18,26 @@ s1 <- EUtilsSummary(search,
                       retmax = n,
                       mindate = start, 
                       maxdate = end)
+
+comment <- glue::glue("Please wait...Your query is ", {s1@querytranslation}, ". This returns ", {s1@count}, " abstracts. ", 
+                      "By default 1000 abstracts are downloaded. You downloaded", n, " abstracts. To retrieve more set 'n =' argument to the desired value")
+
+print(comment)
   
 fetch <- EUtilsGet(s1, type = "efetch", db = "pubmed")
-  
-abstracts <- data.frame(title = fetch@ArticleTitle,
-                          abstract = fetch@AbstractText, 
-                          journal = fetch@Title,
-                          DOI = fetch@PMID, 
-                          year = fetch@YearPubmed) %>%
-    mutate(abstract = as.character(abstract))
 
-comment <- glue::glue("Your query is ", {s1@querytranslation}, ". This returns ", {s1@count}, " abstracts. ", 
-           "By default 1000 abstracts are downloaded. To retrieve more set 'n =' argument to the desired value")
+abstracts <- bibliometrix::pubmed2df(fetch)
+  
+# abstracts <- data.frame(title = fetch@ArticleTitle,
+#                           abstract = fetch@AbstractText, 
+#                           journal = fetch@Title,
+#                           DOI = fetch@PMID, 
+#                           year = fetch@YearPubmed) %>%
+#     mutate(abstract = as.character(abstract))
+
 
 
 ## returns latest 1000 abstracts unless n value changed   
-print(comment)
 abstracts           
 }
 
