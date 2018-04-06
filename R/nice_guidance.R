09e252cafc80410bcd2eb8a12bbc383d13d3064f
julianflowers <julianflowers@users.noreply.github.com>
2018-04-06 10:13:00 +0100

    Update nice_guidance.R

--- R/nice_guidance.R
+++ R/nice_guidance.R
@@ -55,17 +55,4 @@ get_nice_ph_guidance <- function(url){
 
 
 
-url_list <- paste0("https://www.nice.org.uk/guidance/ph", c(1,3, 5:32, 35:36, 38:56))
 
-url_ng <- paste0("https://www.nice.org.uk/guidance/ng", c(6:7, 13, 16, 30, 32, 34, 44, 48, 55, 58, 60, 63:64, 68, 70))
-urls <- c(url_list, url_ng)
-guidance = str_extract(urls, "ph[0-9].*$|ng[0-9].*$")
-nice_guidance <- purrr::map_df(urls, function(x) get_nice_ph_guidance(x)) 
-
-
- nice_guidance %>%
-  mutate(published = ifelse(str_detect(date, "Last"), date, NA )) %>%
-  mutate(date = lag(date)) %>%
-  filter(stringr::str_detect(rec, "^ Next")) %>%
-  View()
-         
