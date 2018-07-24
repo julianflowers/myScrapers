bf6ac4eab3d8fe8a9acdad92aba57aaab0f1e9e2
Julian Flowers <julian.flowers12@gmail.com>
2018-07-24 16:15:59 +0100

    latest updates

---
+++ R/get_dsph_list.R
@@ -0,0 +1,29 @@
+
+
+get_dsph_england <- function(url){
+  
+  require(tidyverse); require(rvest)
+  url <- "https://www.gov.uk/government/publications/directors-of-public-health-in-england--2/directors-of-public-health-in-england"
+  
+  dsph <- read_html(url)
+  
+  dsph <- dsph %>%
+    html_nodes("p") %>%
+    html_text() %>%
+    .[10:161] %>%
+    data.frame() 
+  
+  
+  colnames(dsph) <- "data"
+  
+  dsph <- dsph %>%
+    mutate(data = str_replace(data, "Tameside", "Tameside -")) %>%
+    mutate(data = str_replace(data, "–  Wendy", "- Wendy")) %>%
+    mutate(data = str_replace(data, " -Imran", " - Imran")) %>%
+    mutate(data = str_replace(data, "Meredith \\(actin", "Meredith \\(acting")) %>%
+    separate(data, c("LA", "Name"), sep = " - ")
+  
+}
+
+dsph <- get_dsph_england()
+dsph
