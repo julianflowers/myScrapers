f775a9b8f0232c9def3c27562b4eb837b0e8bca6
Julian Flowers <julian.flowers12@gmail.com>
2018-08-14 07:54:16 +0100

    google search function

---
+++ R/googlesearchR.R
@@ -0,0 +1,34 @@
+# A script to search google
+
+googlesearchR <- function(search, n = 100 ){
+
+  require(stringr)
+  require(rvest)
+  require(httr)
+  require(purrr)
+  
+  search <- str_replace_all(search, " ", "+")
+  
+  u <- paste0("https://www.google.co.uk/search?q=", search, "&num=", n )
+  
+  html <- GET(u)
+  
+  doc <- read_html(html)
+  
+  attrs <- html_nodes(doc, "a") %>% html_attr("href")
+  
+  #links <- grep("http://", attrs, fixed = TRUE, value=TRUE)
+  links <- str_replace_all(attrs, "&sa=.+|%3.+", "")
+  links <- str_replace_all(links, "\\/url\\?q=", "")
+  links <- str_extract_all(links, "^http.+")
+  links <- str_replace_all(links, "http://webcache.googleusercontent.com/search", "") 
+  links <- str_extract_all(links, "^http.+")
+  links <- flatten(links)
+  links <- links[12:length(links)]
+  
+  return(links)
+  
+}
+
+googlesearchR("director of public health annual report", n = 400)
+
