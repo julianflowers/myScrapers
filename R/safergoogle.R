5b638e2010ebfcd767518fd361f2ae4c3a65dfbe
Julian Flowers <julian.flowers12@gmail.com>
2019-03-15 16:40:55 +0000

    safergoogle

---
+++ R/safergoogle.R
@@ -0,0 +1,11 @@
+#Safer google search (uses safely and and Sys.sleep())
+
+
+
+safergoogle <- function(search = search, sleep = 5){
+  
+  safergoogle <- safel(googlesearchR)
+  search <- safergoogle(search)
+  Sys.sleep(sleep)
+  
+}
