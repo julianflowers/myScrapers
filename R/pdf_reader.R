3e2310197cee210ddc17badecd810fe49c11d30b
julianflowers <julianflowers@users.noreply.github.com>
2019-04-04 07:56:12 +0100

    Update pdf_reader.R

--- R/pdf_reader.R
+++ R/pdf_reader.R
@@ -1,12 +1,12 @@
 ## pdf reader - saves pdf files as a series of images
 
-pdf_reader <- function(pdf){
+pdf_reader <- function(pdf, path){
   require(pdftools)
   require(magick)
   for(page in 1:pdf_info(pdf)$pages){
     pdftools::pdf_render_page(pdf, page = page) %>%
       image_read() %>%
-      image_write(paste0("page", page, ".png"))
+      image_write(paste0(path, "/page", page, ".png"))
     
   }
 }  
