ed12e574565bd1a3ec9d37267d198f26d11c3126
Julian Flowers <julian.flowers@phe.gov.uk>
2019-04-22 09:07:32 +0100

    test

--- R/get_pqs_health.R
+++ R/get_pqs_health.R
@@ -2,7 +2,7 @@
 ## 
 
 
-get_pqs_health <- function(start_date){
+get_pqs_health <- function(start_date, end_date){
   
   require(hansard)
   require(tidyverse)
@@ -10,7 +10,7 @@ get_pqs_health <- function(start_date){
   
   cat("Please wait...")
   
-  dhsc2018 <- hansard_all_answered_questions(start_date = start_date) %>% dplyr::filter(str_detect(answering_body, "[Hh]ealth"))
+  dhsc2018 <- hansard_all_answered_questions(start_date = start_date, end_date = end_date) %>% dplyr::filter(str_detect(answering_body, "[Hh]ealth"))
   phe_qn <- dplyr::filter(dhsc2018) %>%
     dplyr::select(answer_date = date_of_answer_value, question_text,  answer_text = answer_text_value, answering_member = answering_member_printed_value,
            hansard_category = hansard_heading_value) %>%
@@ -21,3 +21,5 @@ get_pqs_health <- function(start_date){
 }
 
 
+
+  
