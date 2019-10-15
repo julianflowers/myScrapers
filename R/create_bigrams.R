9bb6b25bb083db1987a24f3292c75774b224ff91
Julian Flowers <julianflowers@julians-mbp.home>
2019-10-15 21:11:24 +0100

    add bigrams

---
+++ R/create_bigrams.R
@@ -0,0 +1,12 @@
+create_bigrams <- function(df, text){
+  require(tidytext)
+  require(dplyr)
+  require(Matrix)
+  text <- enquo(text)
+  df %>%
+    unnest_tokens(bigram, !!text, token = "ngrams", n = 2) %>%
+    separate(bigram, c("w1", "w2"), sep = " ") %>%
+    dplyr::filter(!w1 %in% stop_words$word) %>%
+    dplyr::filter(!w2 %in% stop_words$word) %>%
+    unite(bigram, c("w1", "w2"), sep = " ")
+}
