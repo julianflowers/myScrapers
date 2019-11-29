2fa662368709b997b2cc231a98565cc14424afcf
Julian Flowers <julianflowers@julians-mbp.home>
2019-11-29 11:06:19 +0000

    correction

--- R/get_pqs.R
+++ R/get_pqs.R
@@ -7,10 +7,12 @@ get_phe_pqs <- function(start_date){
   if(!require(hansard))devtools::install_github("EvanOdell/hansard")
   require(hansard)
   require(tidyverse)
+  require(dplyr)
 
   cat("Please wait...")
   
   dhsc2018 <- all_answered_questions(start_date = start_date, answering_body = "health and social care") 
+  
   phe_qn <- dplyr::filter(dhsc2018, str_detect(answer_text_value, "Public Health England|PHE")) %>%
     dplyr::select(answer_date = date_of_answer_value, question_text,  answer_text = answer_text_value, answering_member = answering_member_printed_value,
            hansard_category = hansard_heading_value) %>%
@@ -21,4 +23,3 @@ get_phe_pqs <- function(start_date){
   
 }
 
-pq <- get_phe_pqs("2019-09-01")
