32a56183686041b23d26c224dfa19496c2d43c76
julianflowers <julianflowers@users.noreply.github.com>
2020-02-04 11:28:59 +0000

    update

--- R/annotate_abstracts.R
+++ R/annotate_abstracts.R
@@ -8,7 +8,7 @@ annotate_abstracts <- function(abstract, pmid){
   
   ud_model <- udpipe_load_model(ud_model$file_model)
   
-  x <- udpipe_annotate(ud_model, x = out$abstracts$absText, doc_id = out$abstracts$pmid)
+  x <- udpipe_annotate(ud_model, x = abstract, doc_id = pmid)
   x <- as.data.frame(x)
   x$topic_id <- unique_identifier(x, fields = c("doc_id", "paragraph_id", "sentence_id"))
   
