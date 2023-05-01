27d682b58cb37d4dcc2351c1d0b50b070807d4a4
julianflowers <julianflowers@users.noreply.github.com>
2023-05-01 14:34:34 +0100

    Update get_page_csv.R

--- R/get_page_csv.R
+++ R/get_page_csv.R
@@ -21,7 +21,7 @@ get_page_csvs <- function(url){
     read_html() %>%
     html_nodes("a") %>%
     html_attr("href") %>%
-    .[grepl("csv$|xls.$", .)]
+    .[grepl("csv$|xls$|xlsx$|ods$", .)]
     
   
   return(csv)
