fe46ae951e28b19647677edbd2105cff2732c5ae
julianflowers12 <julian.flowers13@gmail.com>
2024-08-03 08:48:56 +0100

    update

--- R/get_ss.R
+++ R/get_ss.R
@@ -1,5 +1,5 @@
 ## get semantic scholar abstracts
-get_ss_data <- function(n = 100, search, journal = NULL){
+get_ss_data <- function(n = 100, search, journal = NULL, api_key, mindate = 2020){
 
 
   require(tidyverse)
@@ -9,7 +9,8 @@ get_ss_data <- function(n = 100, search, journal = NULL){
   search_term <- str_replace_all(search, "\\s", "+")
   journal = journal
 
-  uri <- glue::glue("https://api.semanticscholar.org/graph/v1/paper/search?query=", {search_term}, "&offset=10&limit=", n, "&fields=paperId,externalIds,title,abstract,year,venue,authors,url,referenceCount,citationCount,influentialCitationCount,isOpenAccess")
+  uri <- glue::glue("https://api.semanticscholar.org/graph/v1/paper/search/bulk?query=", {search_term}, "&", 
+                    {api_key}, "&year=", {mindate}, "-",  "&offset=10&limit=", n, "&fields=paperId,externalIds,title,abstract,year,venue,authors,url,referenceCount,citationCount,influentialCitationCount,isOpenAccess")
 
 
   df <- jsonlite::fromJSON(uri, simplifyDataFrame = TRUE)
@@ -21,3 +22,6 @@ get_ss_data <- function(n = 100, search, journal = NULL){
 }
 
 
+api_key <- Sys.getenv("SEMANTICSCHOLAR_API")
+test <- get_ss_data(search = 'maternal mortality+saudi+arabia', api_key = api_key, mindate = 2010, n = 1000)
+test$data[18]
